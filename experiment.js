ccfa78bdc6a814ef173ca12bcd4462a5634859c1
Rob Olson <rholson1@wisc.edu>
2024-01-17 13:26:25 -0600

    Add jsPsych and experiment files, including code incorporated into qualtrics question (html and js).

---
+++ experiment.js
@@ -0,0 +1,169 @@
+
+
+
+var g = {};  // put everything in a namespace variable to simplify cleanup, avoid name collisions
+
+g.repo = "https://sj1118.github.io/Emotion-Color-Study/";
+
+
+/* 6 blocks of 10 trials
+Each block corresponds to a single emotion.
+
+Each trial consists of:
+500 ms fixation cross
+500 ms blank screen
+display image pair (until a response is received)
+
+image pair consists of:
+one image where expression and coloration are congruent
+one image where expression and coloration are incongruent
+
+
+Randomization:
+order of blocks (emotions)
+congruent image side
+
+
+ */
+
+// Emotions which get a block
+g.emotions = [
+    { code: 2, emotion: 'Happy', emotion_label: 'Happy' },
+    { code: 3, emotion: 'Sad', emotion_label: 'Sad' },
+    { code: 4, emotion: 'Angry', emotion_label: 'Angry' },
+    { code: 5, emotion: 'Surprised', emotion_label: 'Surprised' },
+    { code: 6, emotion: 'Disgusted', emotion_label: 'Disgusted' },
+    { code: 15, emotion: 'Surprisely_fearful', emotion_label: 'Fearfully surprised' }  // "fearfully surprised"
+]
+
+// emotion codes (currently includes additional emotions)
+g.coloration = [2, 3, 4, 5, 6, 8, 9, 15];
+
+g.subjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11];
+
+// stimulus has form: subject_{subject number}/{Emotion}/sub{subject number}_{expression}_{coloration}.png
+
+// Generate timeline
+
+// shuffle emotions (randomize block order)
+g.emotions = jsPsych.randomization.shuffle(g.emotions);
+
+g.blocks = [];
+for (let i=0; i < g.emotions.length; i++) {
+
+    let block = [];
+    // 10 trials in the block, one from each subject
+    let cong_on_left = jsPsych.randomization.repeat([true, false], 5);
+
+    for (let j=0; j < 2; j++) {  // j < 10
+
+        let prefix = 'subject_' + g.subjects[j] + '/' + g.emotions[i].emotion + '/sub' + g.subjects[j] + '_' + g.emotions[i].code + '_'
+        let coloration = g.emotions[i].code;
+        while (coloration === g.emotions[i].code) {
+            coloration = jsPsych.randomization.sampleWithoutReplacement(g.coloration, 1)[0];
+        }
+
+        block.push({
+            congruent: prefix + g.emotions[i].code + '.png',
+            incongruent: prefix + coloration + '.png',
+            congruent_on_left: cong_on_left[j]
+        });
+    }
+    g.blocks.push({
+        emotion: g.emotions[i].emotion,
+        emotion_label: g.emotions[i].emotion_label,
+        block: block
+    });
+}
+
+g.timeline = [];
+
+g.welcome = {
+    type: "fullscreen",
+    message: "Now you are entering full screen mode.<br>",
+    button_label: "Click here to proceed.",
+    delay_after: 500
+}
+
+g.fixation = {
+    type: 'html-keyboard-response',
+    stimulus: '<div class="container"><div class="fixation">+</div></div>',
+    choices: jsPsych.NO_KEYS,
+    trial_duration: 500
+};
+
+g.blank = {
+    type: 'html-keyboard-response',
+    stimulus: '<div class="container"></div>',
+    choices: jsPsych.NO_KEYS,
+    trial_duration: 500
+}
+
+g.trial = {
+    type: 'html-keyboard-response',
+    stimulus: function() {
+        let leftimg;
+        let rightimg;
+        if (jsPsych.timelineVariable('congruent_on_left', true)) {
+            leftimg = jsPsych.timelineVariable('congruent', true);
+            rightimg = jsPsych.timelineVariable('incongruent', true);
+        } else {
+            leftimg = jsPsych.timelineVariable('incongruent', true);
+            rightimg = jsPsych.timelineVariable('congruent', true);
+        }
+        let string_parts = [];
+        string_parts.push('<div class="container"><div class="faceimg"><img alt="human_face" src="' + leftimg + '"></div>');
+        string_parts.push('<div class="faceimg"><img alt="human_face" src="' + rightimg + '"></div></div>');
+        return string_parts.join('')
+    },
+    choices: ['ArrowLeft', 'ArrowRight'],
+    prompt: function(){
+        let emotion = jsPsych.timelineVariable('emotion_label');
+        return 'Which face looks more <b>' + emotion + '</b>?<br>Press the left or right arrow key.'
+    },
+    data: {
+        congruent_on_left: jsPsych.timelineVariable('congruent_on_left'),
+        congruent: jsPsych.timelineVariable('congruent'),
+        incongruent: jsPsych.timelineVariable('incongruent')
+    },
+    on_finish: function(data) {
+        if (data.response === 'arrowleft') {
+            data.selected_side = 'left';
+        } else if(data.response === 'arrowright') {
+            data.selected_side = 'right';
+        } else {
+            console.log(data.response);
+        }
+
+        if ((data.congruent_on_left && data.response === 'arrowleft') ||
+            (!data.congruent_on_left && data.response === 'arrowright')) {
+                data.selected_img = 'congruent';
+            } else {
+                data.selected_img = 'incongruent';
+            }
+    }
+}
+
+g.timeline.push(g.welcome);
+
+// build timeline
+g.blocks.forEach(function(block){
+    g.timeline.push({
+        type: 'html-keyboard-response',
+        stimulus: 'This block will concern the emotion "' + block.emotion_label + '"',
+        prompt: 'Press any key to continue',
+        choices: jsPsych.ALL_KEYS
+    });
+    g.timeline.push({
+        timeline: [{
+            timeline: [g.fixation, g.blank, g.trial],
+            timeline_variables: block.block
+        }],
+        timeline_variables: [{ emotion_label: block.emotion_label }]
+    });
+})
+
+
+
+
+
---
+++ qualtrics.js
@@ -0,0 +1,113 @@
+Qualtrics.SurveyEngine.addOnload(function () {
+
+    /*Place your JavaScript here to run when the page loads*/
+
+    /* Change 1: Hiding the Next button */
+    // Retrieve Qualtrics object and save in qthis
+    var qthis = this;
+
+    // Hide buttons
+    qthis.hideNextButton();
+
+    /* Change 2: Defining and load required resources */
+    var task_github = "https://sj1118.github.io/Emotion-Color-Study/"; // https://<your-github-username>.github.io/<your-experiment-name>
+
+    // requiredResources must include all the JS files that the task uses.
+    var requiredResources = [
+
+        task_github + "lib/jspsych-6.3.1/jspsych.js",
+        task_github + "lib/jspsych-6.3.1/plugins/jspsych-html-keyboard-response.js",
+        task_github + "lib/jspsych-6.3.1/plugins/jspsych-fullscreen.js",
+        task_github + "experiment.js"
+    ];
+
+    function loadScript(idx) {
+        console.log("Loading ", requiredResources[idx]);
+        jQuery.getScript(requiredResources[idx], function () {
+            if ((idx + 1) < requiredResources.length) {
+                loadScript(idx + 1);
+            } else {
+                initExp();
+            }
+        });
+    }
+
+    if (window.Qualtrics && (!window.frameElement || window.frameElement.id !== "mobile-preview-view")) {
+        loadScript(0);
+    }
+
+    /* Change 3: Appending the display_stage Div using jQuery */
+    // jQuery is loaded in Qualtrics by default
+    jQuery("<div id = 'display_stage_background'></div>").appendTo('body');
+    jQuery("<div id = 'display_stage'></div>").appendTo('body');
+
+
+    var task_name = "emotion-color-study";
+    var save_url = "https://experiment.childemotion.waisman.wisc.edu/save_data.php";
+
+    // child and response supplied by Lookit
+    var child_id = "${e://Field/child_id}";
+    var response_id = "${e://Field/response_id}";
+
+    //var subject_id = "${e://Field/participantID}";
+    var data_dir = task_name;
+    var file_name = task_name + '_' + child_id + '-' + response_id;
+
+    function save_data_json() {
+        jQuery.ajax({
+            type: 'post',
+            cache: false,
+            url: save_url,
+            data: {
+                data_dir: data_dir,
+                file_name: file_name + '.json', // the file type should be added
+                exp_data: jsPsych.data.get().json()
+            }
+        });
+    }
+
+    function save_data_csv() {
+        jQuery.ajax({
+            type: 'post',
+            cache: false,
+            url: save_url,
+            data: {
+                data_dir: data_dir,
+                file_name: file_name + '.csv', // the file type should be added
+                exp_data: jsPsych.data.get().csv()
+            }
+        });
+    }
+
+    /* Change 4: Wrapping jsPsych.init() in a function */
+    function initExp() {
+
+        jsPsych.init({
+            timeline: g.timeline,
+            //preload_images: g.preload_images,
+            display_element: 'display_stage',
+            on_finish: function (data) {
+
+                jsPsych.data.get().addToAll({child_id: child_id, response_id: response_id});
+                save_data_csv();
+
+                // clear the stage
+                jQuery('#display_stage').remove();
+                jQuery('#display_stage_background').remove();
+
+                // simulate click on Qualtrics "next" button, making use of the Qualtrics JS API
+                qthis.clickNextButton();
+            }
+        });
+    }
+});
+
+Qualtrics.SurveyEngine.addOnReady(function () {
+    /*Place your JavaScript here to run when the page is fully displayed*/
+
+});
+
+Qualtrics.SurveyEngine.addOnUnload(function () {
+    /*Place your JavaScript here to run when the page is unloaded*/
+    g = undefined;
+});
